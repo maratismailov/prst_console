740133935f9f3f1d5f30e4130235f36a8684fb46
regfe89 <marat.ismailov889@gmail.com>
2021-09-07 13:26:11 +0600

    png commented out

--- save_png.cpp
+++ save_png.cpp
@@ -42,93 +42,93 @@ void png_io_flush(png_structp png_ptr)
   out->flush();
 }
 
-bool Save_PNG(const string & filename, byte *image_data, unsigned width, unsigned height, PNG_COMPRESSION_T compression)
-{
-	int z_compression;
+// bool Save_PNG(const string & filename, byte *image_data, unsigned width, unsigned height, PNG_COMPRESSION_T compression)
+// {
+// 	int z_compression;
 
-	switch (compression)
-	{
-		case PNG_COMPRESSION_NONE:	z_compression = 0;	break;
-		case PNG_COMPRESSION_FAST:	z_compression = 1;	break;
-		case PNG_COMPRESSION_BEST:	z_compression = 9;	break;
-		default:
-			z_compression = 0;
-			Fatal_Error( "Unknown compression level for saving a screenshot" );
-	}
+// 	switch (compression)
+// 	{
+// 		case PNG_COMPRESSION_NONE:	z_compression = 0;	break;
+// 		case PNG_COMPRESSION_FAST:	z_compression = 1;	break;
+// 		case PNG_COMPRESSION_BEST:	z_compression = 9;	break;
+// 		default:
+// 			z_compression = 0;
+// 			Fatal_Error( "Unknown compression level for saving a screenshot" );
+// 	}
 
-	// Open PNG file for writing
-	ofstream* out = new ofstream(filename.c_str(), ios_base::binary);
-	if (!out)
-		return false;
+// 	// Open PNG file for writing
+// 	ofstream* out = new ofstream(filename.c_str(), ios_base::binary);
+// 	if (!out)
+// 		return false;
 
-	// Allocate basic libpng structures
-	png_structp png_ptr = png_create_write_struct(PNG_LIBPNG_VER_STRING, 0, 0, 0);
-	if (!png_ptr)
-	{
-		out->close();
-		delete out;
-		return false;
-	}
+// 	// Allocate basic libpng structures
+// 	png_structp png_ptr = png_create_write_struct(PNG_LIBPNG_VER_STRING, 0, 0, 0);
+// 	if (!png_ptr)
+// 	{
+// 		out->close();
+// 		delete out;
+// 		return false;
+// 	}
 
-	png_infop info_ptr = png_create_info_struct(png_ptr);
-	if (!info_ptr)
-	{
-		png_destroy_write_struct(&png_ptr, 0);
-		out->close();
-		delete out;
-		return false;
-	}
+// 	png_infop info_ptr = png_create_info_struct(png_ptr);
+// 	if (!info_ptr)
+// 	{
+// 		png_destroy_write_struct(&png_ptr, 0);
+// 		out->close();
+// 		delete out;
+// 		return false;
+// 	}
 
-	// setjmp() must be called in every function
-	// that calls a PNG-reading libpng function
-	if (setjmp(png_jmpbuf(png_ptr)))
-	{
-		png_destroy_write_struct(&png_ptr, &info_ptr);
-		out->close();
-		delete out;
-		return false;
-	}
+// 	// setjmp() must be called in every function
+// 	// that calls a PNG-reading libpng function
+// 	if (setjmp(png_jmpbuf(png_ptr)))
+// 	{
+// 		png_destroy_write_struct(&png_ptr, &info_ptr);
+// 		out->close();
+// 		delete out;
+// 		return false;
+// 	}
 
-	// set the zlib compression level
-	png_set_compression_level(png_ptr, z_compression);
+// 	// set the zlib compression level
+// 	png_set_compression_level(png_ptr, z_compression);
 
-	png_set_write_fn(png_ptr, out, png_write_data, png_io_flush);
+// 	png_set_write_fn(png_ptr, out, png_write_data, png_io_flush);
 
-	// write PNG info to structure
-	png_set_IHDR(	png_ptr,						info_ptr,
-					width,							height,					8,
-					PNG_COLOR_TYPE_RGB,				PNG_INTERLACE_NONE,
-					PNG_COMPRESSION_TYPE_DEFAULT,	PNG_FILTER_TYPE_DEFAULT		);
+// 	// write PNG info to structure
+// 	png_set_IHDR(	png_ptr,						info_ptr,
+// 					width,							height,					8,
+// 					PNG_COLOR_TYPE_RGB,				PNG_INTERLACE_NONE,
+// 					PNG_COMPRESSION_TYPE_DEFAULT,	PNG_FILTER_TYPE_DEFAULT		);
 
-	png_write_info(png_ptr, info_ptr);
+// 	png_write_info(png_ptr, info_ptr);
 
-	// Allocate pointers for each line
-	png_bytepp row_pointers = new png_bytep[height];
-	if (!row_pointers)
-	{
-		png_destroy_write_struct(&png_ptr, &info_ptr);
-		out->close();
-		delete out;
-		return false;
-	}
+// 	// Allocate pointers for each line
+// 	png_bytepp row_pointers = new png_bytep[height];
+// 	if (!row_pointers)
+// 	{
+// 		png_destroy_write_struct(&png_ptr, &info_ptr);
+// 		out->close();
+// 		delete out;
+// 		return false;
+// 	}
 
-	// how many bytes in each row
-	png_uint_32 rowbytes = png_get_rowbytes(png_ptr, info_ptr);
+// 	// how many bytes in each row
+// 	png_uint_32 rowbytes = png_get_rowbytes(png_ptr, info_ptr);
 
-	// set the individual row_pointers to point at the correct offsets
-	for (png_uint_32 i = 0; i < height; ++i)
-		row_pointers[i] = (png_byte*)(image_data + i * rowbytes);
+// 	// set the individual row_pointers to point at the correct offsets
+// 	for (png_uint_32 i = 0; i < height; ++i)
+// 		row_pointers[i] = (png_byte*)(image_data + i * rowbytes);
 
-	// now we can go ahead and just write the whole image
-	png_write_image(png_ptr, row_pointers);
+// 	// now we can go ahead and just write the whole image
+// 	png_write_image(png_ptr, row_pointers);
 
-	png_write_end(png_ptr, 0);
+// 	png_write_end(png_ptr, 0);
 
-	png_destroy_write_struct(&png_ptr, &info_ptr);
+// 	png_destroy_write_struct(&png_ptr, &info_ptr);
 
-	delete [] row_pointers;
+// 	delete [] row_pointers;
 
-	out->close();
-	delete out;
-	return true;
-}
+// 	out->close();
+// 	delete out;
+// 	return true;
+// }
